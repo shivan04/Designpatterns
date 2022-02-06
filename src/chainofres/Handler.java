91852f56740c0e0b2782b2341ebce03cbd6a7302
shivan <s.sewdien@unasat.sr>
2022-02-05 22:29:04 -0300

    Initial commit

---
+++ src/chainofres/Handler.java
@@ -0,0 +1,21 @@
+package chainofres;
+
+public abstract class Handler {
+    private Handler next;
+    public Handler setNextHandler(Handler next){
+
+        this.next= next;
+        return next;
+    }
+
+    public abstract boolean handle (String username , String password);
+
+    protected boolean handleNext(String username , String passsword){
+        if (next==null){
+            return true;
+        }
+        return next.handle(username, passsword);
+    }
+
+
+}
